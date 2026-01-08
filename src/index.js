476213dbb9dd26fed3ceb92246e2880e8c5ccb6b
Tanjimul <ayantanjim0@gmail.com>
2026-01-09 01:31:36 +0600

    feat: refactor database connection logic to handle success and error cases

--- src/index.js
+++ src/index.js
@@ -7,8 +7,15 @@ dotenv.config({
 
 
 
-connectDB();
-
+connectDB()
+.then(() => {
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running at port : ${process.env.PORT}`);
+  })
+})
+.catch((err)=>{
+  console.log(`mongoDB connection failed !!!`, err);
+})
 
 
 
